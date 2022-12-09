34095f79ace1ed294e462eee12d9c9117463969e
tmSheak <tmsheak@gmail.com>
2022-12-09 15:46:01 -0500

    This is the First Commit

---
+++ src/com/java/IntroJava.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class IntroJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bismillah!");
+		System.out.println("Trying to Learn Java Program.");
+		System.out.println("I am not sure what to write.");
+		System.out.println("There will no harm even I make mistake.");
+		System.out.println("Let me trying to writing  my Java code.");
+		System.out.println("I started --------------");
+	
+			
+
+	}
+
+}
